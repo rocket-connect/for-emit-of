3c976185fe2e90af61a9ddabf8469ee44a8eded0
Daniel Starns <danielstarns@hotmail.com>
2020-04-13 18:02:59 +0100

    Test: change the structure

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,47 +11,51 @@ describe("forEmitOf", () => {
     expect(forEmitOf).to.be.a("function");
   });
 
-  it("should throw emitter must be a instance of EventEmitter", () => {
-    const str = "i am a string";
+  describe("validation", () => {
+    it("should throw emitter must be a instance of EventEmitter", () => {
+      const str = "i am a string";
 
-    try {
-      // @ts-ignore
-      forEmitOf(str);
+      try {
+        // @ts-ignore
+        forEmitOf(str);
 
-      throw new Error();
-    } catch ({ message }) {
-      expect(message)
-        .to.be.a("string")
-        .to.equal("emitter must be a instance of EventEmitter");
-    }
+        throw new Error();
+      } catch ({ message }) {
+        expect(message)
+          .to.be.a("string")
+          .to.equal("emitter must be a instance of EventEmitter");
+      }
+    });
+
+    it("should throw stream has ended", async () => {
+      const read = fs.createReadStream(path.join(__dirname, "../package.json"));
+
+      try {
+        // @ts-ignore
+        read.readableEnded = true;
+
+        forEmitOf(read);
+
+        throw new Error();
+      } catch ({ message }) {
+        expect(message).to.be.a("string").to.equal("stream has ended");
+      }
+    });
   });
 
-  it("should throw stream has ended", async () => {
-    const read = fs.createReadStream(path.join(__dirname, "../package.json"));
+  describe("functionality", () => {
+    it("should return a iterator and iterate a string", async () => {
+      const read = Readable.from("test");
 
-    try {
-      // @ts-ignore
-      read.readableEnded = true;
+      const iterator = forEmitOf<string>(read);
 
-      forEmitOf(read);
+      let result = "";
 
-      throw new Error();
-    } catch ({ message }) {
-      expect(message).to.be.a("string").to.equal("stream has ended");
-    }
-  });
+      for await (const chunk of iterator) {
+        result += chunk;
+      }
 
-  it("should return a iterator and iterate a string", async () => {
-    const read = Readable.from("test");
-
-    const iterator = forEmitOf<string>(read);
-
-    let result = "";
-
-    for await (const chunk of iterator) {
-      result += chunk;
-    }
-
-    expect(result).to.equal("test");
+      expect(result).to.equal("test");
+    });
   });
 });
