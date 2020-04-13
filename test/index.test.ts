0741786574ac3f1c482c52693f49390aee658bcf
Daniel Starns <danielstarns@hotmail.com>
2020-04-13 15:06:21 +0100

    Test: add test to check of stream has ended

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,14 @@
 import { expect } from "chai";
 import { describe } from "mocha";
 import forEmitOf from "../src";
+import { Readable } from "stream";
 
 describe("forEmitOf", () => {
   it("should be a function", () => {
     expect(forEmitOf).to.be.a("function");
   });
 
-  it("should throw emitter must be a instance of EventEmitter", async () => {
+  it("should throw emitter must be a instance of EventEmitter", () => {
     const str = "i am a string";
 
     try {
@@ -20,6 +21,22 @@ describe("forEmitOf", () => {
       expect(message)
         .to.be.a("string")
         .to.equal("emitter must be a instance of EventEmitter");
+    }
+  });
+
+  it("should throw stream has ended", async () => {
+    const read = Readable.from("test");
+
+    const buffers: any[] = [];
+
+    for await (const buff of read) {
+      buffers.push(buff);
+    }
+
+    try {
+      forEmitOf(read);
+    } catch ({ message }) {
+      expect(message).to.be.a("string").to.equal("stream has ended");
     }
   });
 });
