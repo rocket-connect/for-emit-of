3abb5a41542adc4cbde4ebd8c4396e4b52210c7b
Daniel Starns <danielstarns@hotmail.com>
2020-04-13 15:06:31 +0100

    Feat: add check to see if stream ended

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import { EventEmitter } from "events";
+import { Readable, Writable } from "stream";
 
 interface Options {
   event?: string;
 }
 
 export default <T>(
-  emitter: EventEmitter,
+  emitter: EventEmitter | Readable | Writable,
   options?: Options
 ): AsyncIterator<T> => {
   if (!(emitter instanceof EventEmitter)) {
     throw new Error("emitter must be a instance of EventEmitter");
   }
+
+  ["readableEnded", "writableEnded"].forEach((key) => {
+    if (key in emitter && Boolean(emitter[key])) {
+      throw new Error("stream has ended");
+    }
+  });
 
   let buffers: T[] = [];
   let error: Error;
