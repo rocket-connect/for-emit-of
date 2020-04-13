d8c7bd8abf3be4c49374662b4f61cff06954a405
Daniel Starns <danielstarns@hotmail.com>
2020-04-13 14:07:49 +0100

    Test: add boilerplate functionality and test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
-function forEmitOf(of: string): string {
-    return of;
+import { EventEmitter } from "events";
+
+interface Options {
+  event?: string;
 }
 
-export default forEmitOf;
+export default <T>(
+  emitter: EventEmitter,
+  options?: Options
+): AsyncIterator<T> => {
+  if (!(emitter instanceof EventEmitter)) {
+    throw new Error("emitter must be a instance of EventEmitter");
+  }
+
+  let buffers: T[] = [];
+  let error: Error;
+  let active = true;
+
+  emitter.on("data", (buff) => buffers.push(buff));
+
+  emitter.on("error", (err) => {
+    error = err;
+    active = false;
+  });
+
+  emitter.on("end", () => {
+    active = false;
+  });
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async function* forEmitOf() {
+    while (buffers.length || active) {
+      if (error) {
+        throw error;
+      }
+
+      const [result, ...restOfBuffers] = buffers;
+
+      buffers = [...restOfBuffers];
+
+      if (!result) {
+        continue;
+      }
+
+      yield result;
+    }
+  }
+
+  const iterator = forEmitOf();
+
+  return iterator;
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { expect } from "chai";
 import { describe } from "mocha";
-import forEmit from "../src";
+import forEmitOf from "../src";
 
 describe("forEmitOf", () => {
-    it("should be a function", () => {
-        expect(forEmit).to.be.a("function");
-    });
+  it("should be a function", () => {
+    expect(forEmitOf).to.be.a("function");
+  });
+
+  it("should throw emitter must be a instance of EventEmitter", async () => {
+    const str = "i am a string";
+
+    try {
+      // @ts-ignore
+      forEmitOf(str);
+
+      throw new Error();
+    } catch ({ message }) {
+      expect(message)
+        .to.be.a("string")
+        .to.equal("emitter must be a instance of EventEmitter");
+    }
+  });
 });
